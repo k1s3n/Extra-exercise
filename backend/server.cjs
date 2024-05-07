2949c8f8cf09080079dc73cc168ae59918eaff84
Johanna Hirvonen <johanna.hirvonen@ludvig.se>
2024-05-07 14:44:43 +0200

    added sqlite database

--- backend/server.cjs
+++ backend/server.cjs
@@ -7,14 +7,30 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('../database/hobbyhub.db');
 
 app.get('/', (req, res) => {
+  db.all('SELECT * FROM person', [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    rows.forEach((row) => {
+      console.log(row.name);
+    });
+  });
   res.send('GET-request mottagen');
+
 });
 
 
 app.post('/', (req, res) => {
   console.log('POST-data:', req.body);
+  db.run('INSERT INTO person (name, info) VALUES (?, ?)', [req.body.name, req.body.info], function(err) {
+    if (err) {
+      console.error(err.message);
+    }
+  });
   res.send('POST-request mottagen');
 });
 
