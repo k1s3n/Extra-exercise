7563e1f2402f2ca29d32241f0b552678b42414e6
k1s3n <christoffer@rehnmark.se>
2024-05-07 11:29:25 +0200

    Feature: get all persons

--- backend/server.cjs
+++ backend/server.cjs
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
   res.send('GET-request mottagen');
 });
 
+app.get('/persons', (req, res) => {
+  const query = 'SELECT * FROM '
+  res.send('GET-request mottagen');
+});
+)
+
 
 app.post('/', (req, res) => {
   console.log('POST-data:', req.body);
